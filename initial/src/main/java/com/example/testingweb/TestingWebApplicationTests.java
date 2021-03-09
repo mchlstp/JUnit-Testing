ad1113886c157bc1b273e58880dfbec6c20c1d3e
mchlstp <michael.h.stump@gmail.com>
2021-03-09 13:31:34 -0600

    initial commit

---
+++ initial/src/main/java/com/example/testingweb/TestingWebApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.testingweb;
+
+import org.testng.annotations.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestingWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
